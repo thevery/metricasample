aa925e0b6e125079113f5e416363f5dd074c3bb6
Ildar Karimov <thevery@yandex-team.ru>
2013-12-24 14:30:03 +0400

    initial version

---
+++ app/src/main/java/com/thevery/metricasample/MyApp.java
@@ -0,0 +1,14 @@
+package com.thevery.metricasample;
+
+import android.app.Application;
+
+import com.yandex.metrica.Counter;
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Counter.initialize(this);
+        Counter.sharedInstance().sendEventsBuffer();
+    }
+}
